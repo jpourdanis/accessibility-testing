2a554df8550e11da53acfd407dfa51c1e4a8884d
John Pourdanis <pourdanisj@vmware.com>
2021-03-15 12:48:27 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# lighthouse pefromance testing
+
+### RUN TESTS
+
+1. Run `npm i`
+2. Run `npm test`
+
+You can find more about the available configuration here :
+https://github.com/GoogleChrome/lighthouse/blob/master/docs/configuration.md
+
+![Lighthouse demo](sample.png)
---
+++ test/lighthouse.test.js
@@ -0,0 +1,63 @@
+const { assert } = require("chai");
+const lighthouse = require("lighthouse");
+const puppeteer = require('puppeteer');
+const Table = require("cli-table");
+const {URL} = require('url');
+
+const config = require("./config.json");
+
+const table = new Table();
+
+// Define your test url.
+const testUrl = "https://the-internet.herokuapp.com/large";
+
+function launchChromeAndRunLighthouse(url, opts, conf = null) {
+  return puppeteer
+    .launch({ 
+      headless: false,
+      executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+      chromeFlags: opts.chromeFlags })
+    .then((brower) => {
+      opts.port = (new URL(brower.wsEndpoint())).port;
+      return lighthouse(url, opts, conf).then((res) =>
+        /** use results.lhr for the JS-consumeable output
+         * use results.report for the HTML/JSON/CSV output as a string
+         * use results.artifacts for the trace/screenshots/other specific case you need (rarer)
+         * https://github.com/GoogleChrome/lighthouse/blob/master/types/lhr.d.ts
+         */
+         brower.close().then(() => res.lhr)
+      );
+    });
+}
+
+const opts = {
+  chromeFlags: ["--disable-gpu", "--show-paint-rects", "--ignore-certificate-errors", "--no-sandbox", "--disable-dev-shm-usage" , "--disable-setuid-sandbox"],
+};
+
+describe("Lighthouse Testing", function () {
+  let results;
+  // Timeout doesn't need to be same. It can be more or less depending on your project.
+  this.timeout(50000);
+  before("run base test", (done) => {
+    launchChromeAndRunLighthouse(testUrl, opts, config).then((res) => {
+      results = Object.keys(res.categories).reduce((merged, category) => {
+        merged[category] = res.categories[category].score;
+        return merged;
+      }, {});
+      done();
+    });
+  });
+
+  it("should have accessibility score greater than 80", (done) => { 
+    assert.equal(results.accessibility > 0.8, true);
+    done();
+  });
+
+  after(() => {
+    Object.keys(results).forEach((category) => {
+      table.push([category, Math.round(results[category] * 100)]);
+    });
+    // Output lighthouse scores in a table format within the cli.
+    console.log(table.toString());
+  });
+});
